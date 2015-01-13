91436c89e6602fb786c978e5aa8439aae7e204e4
Andreas Adelsberger <entwicklung@styleflasher.at>
2015-01-13 09:50:11 +0100

    TranslationExtension activated

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -14,8 +14,8 @@
 
 # this settings you should copy to your siteaccess site.ini
 # so you have the fully control which ts files are loaded
-#[RegionalSettings]
-#TranslationExtensions[]=cjw_newsletter
+[RegionalSettings]
+TranslationExtensions[]=cjw_newsletter
 
 
 [TemplateSettings]
